11f27ab6b74bb089c944e476729568b8b6d50abf
Holly Bieber <habieber1108@gmail.com>
2024-05-02 15:41:25 -0500

    conditional mobile navbar rendering

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,21 @@
 import { Link } from "react-router-dom"
+import { useState } from "react"
 import * as userService from '../../utilities/users-services'
 import { FaBars } from 'react-icons/fa'
 import './NavBar.css'
 
 export default function NavBar({ user, setUser }) {
+  const [nav, setNav] = useState(false)
+
+  const handleNav = () => {
+    setNav(!nav)
+  }
+
   function handleLogOut() {
-    // delegate to the users-service
     userService.logOut();
-    // update state will also cause a re-render
     setUser(null);
   }
+
   return (
     <div className="w-full min-h-[15px] flex justify-between items-center absolute z-10 text-white bg-gray-700/80">
       <ul className="hidden sm:flex px-4">
@@ -25,12 +31,31 @@ export default function NavBar({ user, setUser }) {
             <span>Hello, {user}</span>
           </li>          
           <li>
-          <Link to="">Log Out</Link>
+          <Link to="" onClick={handleLogOut}>Log Out</Link>
           </li>       
           </>
         )} 
-      <div>
-        <FaBars />
+      <div onClick={handleNav} className="sm: z-10">
+        <FaBars size={20} className="mr-4 cursor-pointer" />
+      </div>
+      <div 
+      onClick={handleNav}
+      className={nav ? 'overflow-y-hidden md:hidden ease-in duration-300 absolute text-gray-300 left-0 top-0 w-full h-screen bg-gray-700/80 px-4 py-7 flex flex-col' 
+      : 'absolute top-0 h-screen left-[-100%] ease-in duration-500 '}>
+        <ul className="h-full w-full text-center pt-12">
+          <li className="text-2xl py8">
+            <a href="/">Home</a>
+          </li>
+          <li className="text-2xl py8">
+            <a href="#index">All Dreams</a>
+          </li>
+          <li className="text-2xl py8">
+            <a href="/">Home</a>
+          </li>
+          <li className="text-2xl py8">
+            <a href="/">Home</a>
+          </li>
+        </ul>
       </div>
       </ul>
     </div>
